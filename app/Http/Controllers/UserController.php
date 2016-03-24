a0c8160c4c4790f140d955fe754f51341824e3aa
Juan Carlos <carlox16@gmail.com>
2016-03-24 13:05:35 -0500

    Creando vistas

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,18 @@ use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
+    private $path = "users";
+
+    public function index($name = ""){
+
+        $array = [
+            'name' => $name,
+            'version' => '0.01'
+        ];
+
+        return view($this->path.'.index', $array);
+    }
+
    public function showProfile(){
         return "Hola Mundo";
    }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,8 @@
 <?php
 
 Route::group(['as' => 'user::', 'prefix' => 'user'], function () {
+	
+	Route::get('/{name?}', 'UserController@index')->name('index');
 
     Route::get('/perfil/{id?}', 'UserController@showProfile')->name('profile');
 
